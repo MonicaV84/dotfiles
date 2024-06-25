ea60a1f9ab9b0dac70fb8f23431d91618113265b
Mónica Virginia Ramos Dávila <monicavrd84@gmail.com>
2024-06-25 11:08:28 -0300

    changing plugins and adding keybindings

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,5 +4,8 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
         require("bufferline").setup()
+        vim.keymap.set('n', '<leader>n', ':bn<CR>')
+        vim.keymap.set('n', '<leader>p', ':bp<CR>')
+        vim.keymap.set('n', '<leader>x', ':bd<CR>')
     end,
 }
--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,6 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>")
+		vim.keymap.set("n", "<leader>,", ":Neotree toggle<CR>")
 	end,
 }
